e6dd533c059f67382dda553196dd9d439cfb2374
Mohamed-Ali Turki <moturki@c1r7s3.42lausanne.ch>
2023-11-07 19:31:59 +0100

    pls

--- get_next_line.h
+++ get_next_line.h
@@ -1,25 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moturki <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 10:36:05 by moturki           #+#    #+#             */
+/*   Updated: 2023/10/31 12:48:31 by moturki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
 # include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_list
 {
-	char	buffer[BUFFER_SIZE];
 	int		fd;
-	int		len;
-	int		pos;
-}			t_list;
+	int		curlen;
+	int		size;
+	char	buffer[BUFFER_SIZE];
+}				t_list;
 
 char	*get_next_line(int fd);
 
-int		init_list(t_list *lst, int fd);
-
-char	set_char(t_list *lst);
-
-size_t	ft_strlen(char *str);
+int		ft_strlen(const char *str);
 
 char	*ft_strjoin(char *s1, char s2);
+
+int		init_info(t_list *info, int fd);
+
+char	set_char(t_list *info);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,34 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moturki <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 10:21:35 by moturki           #+#    #+#             */
+/*   Updated: 2023/11/03 14:39:29 by moturki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int	init_list(t_list *lst, int fd)
+int	ft_strlen(const char *str)
 {
-	lst->pos = 0;
-	lst->fd = fd;
-	lst->len = read(lst->fd, lst->buffer, BUFFER_SIZE);
-	if (lst->len < 1)
-		return (0);
-	else
-		return (1);
-}
-
-char	set_char(t_list *lst)
-{
-	char	c;
-
-	if (lst->pos >= lst->len)
-	{
-		lst->pos = 0;
-		lst->len = read(lst->fd, lst->buffer, BUFFER_SIZE);
-		if (lst->len < 1)
-			return (0);
-	}
-	c = lst->buffer[lst->pos++];
-	return (c);
-}
-
-size_t	ft_strlen(char *str)
-{
-	size_t	i;
+	int	i;
 
 	i = 0;
 	if (!str)
@@ -40,20 +26,48 @@ size_t	ft_strlen(char *str)
 
 char	*ft_strjoin(char *s1, char s2)
 {
+	int		len;
+	int		i;
 	char	*str;
-	size_t	i;
-	size_t	j;
 
-	j = 0;
-	i = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 2));
+	len = ft_strlen(s1);
+	str = malloc(sizeof(char) * (len + 2));
 	if (!str)
 		return (NULL);
-	while (i < ft_strlen(s1))
-		str[i++] = s1[j++];
-	str[i] = s2;
-	str[i + 1] = 0;
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i++] = s2;
+	str[i] = 0;
 	if (s1)
 		free(s1);
 	return (str);
+}
+
+int	init_info(t_list *info, int fd)
+{
+	info->curlen = 0;
+	info->fd = fd;
+	info->size = read(info->fd, info->buffer, BUFFER_SIZE);
+	if (info->size < 1)
+		return (0);
+	return (1);
+}
+
+char	set_char(t_list *info)
+{
+	char	c;
+
+	if (info->curlen >= info->size)
+	{
+		info->curlen = 0;
+		info->size = read(info->fd, info->buffer, BUFFER_SIZE);
+		if (info->size < 1)
+			return (0);
+	}
+	c = info->buffer[info->curlen++];
+	return (c);
 }
